a449058cca4c721c282ac0cf135c1fd560817f63
Oleksii Ozyrskyi <aozyrskyii@gmail.com>
2024-12-19 16:29:24 +0300

    reinstall dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "tailwindcss";
 
+/** @type {import('tailwindcss').Config} */
+
 export default defineConfig({
   content: ["./src/**/*.{html,js,ts,jsx,tsx}", "./public/index.html"],
   theme: {
